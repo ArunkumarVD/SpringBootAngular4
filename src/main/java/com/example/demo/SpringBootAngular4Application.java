2905847c1c82ad1e123e61a1141473e59b9979df
arun.d <arunkumar.d303@gmail.com>
2020-07-20 07:10:03 +0530

    Revert "adding new test file"
    This reverts commit b0fa3a4171d28dd911f637fbedacfa5e1d458a6f.

--- src/main/java/com/example/demo/SpringBootAngular4Application.java
+++ src/main/java/com/example/demo/SpringBootAngular4Application.java
@@ -3,12 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 
- * @author Arunt
- * SpringBoot application
- */
-
 @SpringBootApplication
 public class SpringBootAngular4Application {
 
